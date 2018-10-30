e49d76cb14831e99d81408bd6a0008ef64eaa328
Zhang DongYa <zhangdongya@raydonetworks.com>
2018-10-30 17:19:59 +0800

    add newifi 3 (newifi d2) files.

---
+++ trunk/configs/boards/NEWIFI-D2/board.mk
@@ -0,0 +1,9 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# NEWIFI-D2 # LENOVO NEWIFI D2  # MT7621  #
+##################################################################
+
+# Must force use single mac mode.
+CFLAGS += -DBOARD_NEWIFID2 -DVENDOR_LENOVO -DUSE_SINGLE_MAC
+BOARD_NUM_USB_PORTS=1
